8d79a4666fc735b20bb00a938561924e85604c46
pragya chhabra <pragyachhabbra@gmail.com>
2021-08-23 18:54:15 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,14 @@
+document.getElementById("buttonfetch").onclick = async () => {
+const weatherapi = document.getElementById("myinput").value;
+const data = await fetch(`http://api.openweathermap.org/data/2.5/weather
+?q=${weatherapi}&units=metric&APPID=98f8e3fa984b73a9a5e3d531006ad28e`)
+const data2 = await data.json()
+document.getElementById("info").innerHTML = `<ul class="data" style="list-style: none;">
+<li>  <h2 class="city-name">${data2.name}</h2>
+<div class="city-temp">${data2.main.temp}<sup>°C</sup></div>
+<figcaption>${data2.weather[0].description}</li></figcaption>
+</li>
+</ul>`
+
+
+}
