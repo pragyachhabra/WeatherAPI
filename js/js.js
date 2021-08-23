7a18c67407f8d1a3a7e7c8fed6f9625758d51d02
pragya chhabra <pragyachhabbra@gmail.com>
2021-08-23 19:03:49 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 document.getElementById("buttonfetch").onclick = async () => {
 const weatherapi = document.getElementById("myinput").value;
-const data = await fetch(`http://api.openweathermap.org/data/2.5/weather
+const data = await fetch(`https://api.openweathermap.org/data/2.5/weather
 ?q=${weatherapi}&units=metric&APPID=98f8e3fa984b73a9a5e3d531006ad28e`)
 const data2 = await data.json()
 document.getElementById("info").innerHTML = `<ul class="data" style="list-style: none;">
